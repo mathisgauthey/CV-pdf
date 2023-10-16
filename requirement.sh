c976389540de3ddbfdb577a1fcb739871b5ef407
Mathis Gauthey <mathis.gauthey@gmail.com>
2023-10-16 11:31:26 +0200

    Removed global flag from puppeteer install

--- requirement.sh
+++ requirement.sh
@@ -5,6 +5,6 @@ nvm install --lts
 # Install resume-cli globally on your computer
 npm install -g resume-cli
 # Install puppeteer globally on your computer
-PUPPETEER_PRODUCT=firefox npm install -g puppeteer
+PUPPETEER_PRODUCT=firefox npm install puppeteer
 # Install the jsonresume theme on the local project
 npm install jsonresume-theme-onepage-fr
