4cdb48cdbae26eb883cfb49f6647993b3372ccdb
mathisgauthey <mathis.gauthey@gmail.com>
2023-12-13 18:27:06 +0100

    updated requirement

--- requirement.sh
+++ requirement.sh
@@ -4,7 +4,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 nvm install --lts
 # Install resume-cli globally on your computer
 npm install -g resume-cli
-# Install puppeteer globally on your computer
+# Install puppeteer in your project
 PUPPETEER_PRODUCT=firefox npm install puppeteer
 # Install the jsonresume theme on the local project
 npm install jsonresume-theme-onepage-fr
+# WSL specific
+sudo apt install libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2
