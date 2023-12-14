759c91b34737883f64193cbcdf6b07adb8ba068b
mathisgauthey <mathis.gauthey@gmail.com>
2023-12-14 11:15:45 +0100

    edited packages and requirements script accordingly

--- requirement.sh
+++ requirement.sh
@@ -1,15 +1,18 @@
-# GLOBAL PACKAGES -> Managed in the resume repo that contains different CVs
+# GLOBAL
 # Install NVM
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 # Install NPM and Node
 # nvm install --lts
+# WSL specific dependencies
+# sudo apt install libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2
+
+# LOCAL
 # Install resume-cli
 # npm install resume-cli
 # Install puppeteer
 # PUPPETEER_PRODUCT=firefox npm install puppeteer
-# WSL specific dependencies
-# sudo apt install libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2
-
+# Install dependencies from package.json
+# PUPPETEER_PRODUCT=firefox npm install
 
 # LOCAL PACKAGES
 # Install the jsonresume theme
