8a2d9222a296ee3a40ceed8360c2b7933ca8e76f
mathisgauthey <mathis.gauthey@gmail.com>
2023-12-14 10:56:23 +0100

    edited requirements

--- requirement.sh
+++ requirement.sh
@@ -1,12 +1,16 @@
-# Install NVM globally on your computer
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+# GLOBAL PACKAGES -> Managed in the resume repo that contains different CVs
+# Install NVM
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 # Install NPM and Node
-nvm install --lts
-# Install resume-cli globally on your computer
-npm install -g resume-cli
-# Install puppeteer in your project
-PUPPETEER_PRODUCT=firefox npm install puppeteer
-# Install the jsonresume theme on the local project
+# nvm install --lts
+# Install resume-cli
+# npm install resume-cli
+# Install puppeteer
+# PUPPETEER_PRODUCT=firefox npm install puppeteer
+# WSL specific dependencies
+# sudo apt install libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2
+
+
+# LOCAL PACKAGES
+# Install the jsonresume theme
 npm install jsonresume-theme-onepage-fr
-# WSL specific
-sudo apt install libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2
